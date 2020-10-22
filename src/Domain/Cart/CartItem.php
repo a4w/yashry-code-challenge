3f28d97be13920da7c442eb108dbf12a077e4655
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-10-22 18:45:27 +0200

    Add Cart item entity

---
+++ src/Domain/Cart/CartItem.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace Yashry\Domain\Cart;
+
+
+use InvalidArgumentException;
+use Yashry\Domain\Product\Product;
+
+class CartItem
+{
+    private Product $product;
+    private Int $quantity;
+
+    /**
+     * CartItem constructor.
+     * @param Product $product
+     * @param Int $quantity
+     */
+    public function __construct(Product $product, int $quantity)
+    {
+        $this->product = $product;
+        $this->quantity = $quantity;
+    }
+
+    /**
+     * @return Product
+     */
+    public function getProduct(): Product
+    {
+        return $this->product;
+    }
+
+    /**
+     * @return int
+     */
+    public function getQuantity(): int
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * @param int $quantity
+     */
+    public function setQuantity(int $quantity): void
+    {
+        if($quantity < 0){
+            throw new InvalidArgumentException('Quantity cannot be negative');
+        }
+        $this->quantity = $quantity;
+    }
+
+    public function increaseQuantity(){
+        $this->setQuantity($this->quantity+1);
+    }
+
+    public function decreaseQuantity(){
+        $this->setQuantity($this->quantity-1);
+    }
+
+
+}
